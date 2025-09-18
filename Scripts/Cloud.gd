b2558ec48a9788c9226e04826827b01c0aa9b2a5
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-09-18 00:28:01 -0400

    feat: add Cloud.gd script for cloud movement

--- Scripts/Cloud.gd
+++ Scripts/Cloud.gd
@@ -1,11 +1,11 @@
 extends Sprite2D
 
+var speed: float = 100.0
+@export var min_x: float
+@export var max_x: float
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _process(delta):
+	position.x += speed * delta
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if position.x > max_x:
+		position.x = min_x
